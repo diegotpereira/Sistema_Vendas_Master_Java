519a2741f1411e1d600373f2110b02dcff6fea9e
Diego <diegoteixeirapereira@hotmail.com>
2020-09-22 08:54:48 -0300

    Alteração do arquivo classe Conexao.java

--- Sistema_Vendas_Master_Java/src/br/com/java/bd/Conexao.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/bd/Conexao.java
@@ -1,5 +1,18 @@
 package br.com.java.bd;
 
+import java.sql.Connection;
+
 public class Conexao {
+	
+	public final String SERVIDOR = "localhost";
+	public final String PORTA = "3306";
+	public final String BD = "";
+	public final String USUARIO = "";
+	public final String SENHA = "";
+	public final String URL = "";
+	
+	public Connection conexao;
+	
+	
 
 }
