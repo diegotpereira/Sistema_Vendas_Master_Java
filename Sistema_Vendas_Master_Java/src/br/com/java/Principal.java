ef87b7578d854ce952c955abfbc941682d1435f7
Diego <diegoteixeirapereira@hotmail.com>
2020-09-22 08:53:56 -0300

    alteração no arquivo classe Principal.java

--- Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
@@ -5,6 +5,8 @@ package br.com.java;
 public class Principal {
 	
 	 public static void main(String[] args) {
+		 
+		 System.out.println("Criação do projeto para pequenos e medios varejos");
 	        
 	    }
 
