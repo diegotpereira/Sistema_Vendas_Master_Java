a58678763870e1234e5c91d07fc777aea461109d
Diego <diegoteixeirapereira@hotmail.com>
2020-09-22 09:18:06 -0300

    alteração no arquivo classe Conexao.java

--- Sistema_Vendas_Master_Java/src/br/com/java/bd/Conexao.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/bd/Conexao.java
@@ -2,6 +2,7 @@ package br.com.java.bd;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class Conexao {
 	
@@ -14,7 +15,7 @@ public class Conexao {
 	
 	public Connection conexao;
 	
-	public Conexao() {
+	public Conexao() throws SQLException, ClassNotFoundException {
 		
 		try {
 			
@@ -22,11 +23,34 @@ public class Conexao {
 			conexao = DriverManager.getConnection(URL, USUARIO, SENHA);
 			conexao.setAutoCommit(false);
 			
-		} catch (Exception e) {
+		} catch (ClassNotFoundException | IllegalAccessException | InstantiationException e) {
 			// TODO: handle exception
+			throw new ClassNotFoundException("O driver de conexão não foi adicionado ao projeto.\n" + e.getMessage());
+		} catch (SQLException e) {
+			// TODO: handle exception
+			throw new SQLException("Problemas nos parâmetros de conexão.\n" + e.getMessage());
 		}
 		
+	}
+	
+	public Connection getConexao() {
 		
+		return conexao;
+	}
+	
+	public void Confirmar () throws SQLException {
+		
+		try {
+			
+			conexao.commit();
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			throw new SQLException("Problemas na instrução SQL.\n" + e.getMessage());
+		} finally {
+			conexao.close();
+			
+		}
 	}
 
 }
