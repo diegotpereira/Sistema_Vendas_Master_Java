de9b4f67f03e9126fc1ec61a66c6734447f57c04
Diego <diegoteixeirapereira@hotmail.com>
2020-09-23 15:31:23 -0300

    alteração no arquivo classe principal.java

--- Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
@@ -3,15 +3,20 @@ package br.com.java;
 import java.sql.SQLException;
 
 import br.com.java.bd.Conexao;
+import br.com.java.gui.Menu;
 
 public class Principal {
 	
 	 public static void main(String[] args) throws ClassNotFoundException, SQLException {
 		 
-		 Conexao conexao = new Conexao();
+		 //Conexao conexao = new Conexao();
 		 //conexao.setVisible(true);
 		 
-		 System.out.println("Conectado ao banco de dados");
+		 //System.out.println("Conectado ao banco de dados");
+		 
+		 Menu menu =  new Menu();
+		 menu.setVisible(true);
+		 
 	        
 	    }
 
