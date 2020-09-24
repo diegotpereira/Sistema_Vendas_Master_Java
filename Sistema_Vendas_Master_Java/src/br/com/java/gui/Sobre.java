04e3bf6c47db3d9c520e80b1e5cbf6a82a8c416b
Diego <diegoteixeirapereira@hotmail.com>
2020-09-24 14:31:10 -0300

    criação do arquivo classe Sobre.java

--- Sistema_Vendas_Master_Java/src/br/com/java/gui/Sobre.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/gui/Sobre.java
@@ -1,5 +1,16 @@
 package br.com.java.gui;
 
-public class Sobre {
+public class Sobre extends javax.swing.JDialog {
+	
+	 public Sobre(java.awt.Frame parent, boolean modal) {
+	        super(parent, modal);
+	        initComponents();
+	    }
+
+	private void initComponents() {
+		// TODO Auto-generated method stub
+		
+	}
+	
 
 }
