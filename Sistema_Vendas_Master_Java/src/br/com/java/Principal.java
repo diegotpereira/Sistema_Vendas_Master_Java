f2e3148cc91c4ba4d4282c422b3be0ffab5e0409
Diego <diegoteixeirapereira@hotmail.com>
2020-09-24 10:02:32 -0300

    alteração no arquivo Principal.java criação do metodo lookAndFeel

--- Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/Principal.java
@@ -2,6 +2,12 @@ package br.com.java;
 
 import java.sql.SQLException;
 
+import javax.swing.JDialog;
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
 import br.com.java.bd.Conexao;
 import br.com.java.gui.Menu;
 
@@ -13,11 +19,24 @@ public class Principal {
 		 //conexao.setVisible(true);
 		 
 		 //System.out.println("Conectado ao banco de dados");
+		 lookAndFeel();
 		 
 		 Menu menu =  new Menu();
 		 menu.setVisible(true);
 		 
 	        
 	    }
+
+	private static void lookAndFeel() {
+		// TODO Auto-generated method stub
+		 try {
+	            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	            JFrame.setDefaultLookAndFeelDecorated(true);
+	            JDialog.setDefaultLookAndFeelDecorated(true);
+	        } catch (ClassNotFoundException | InstantiationException | IllegalAccessException | UnsupportedLookAndFeelException e) {
+	            JOptionPane.showMessageDialog(null, e, "Erro", JOptionPane.ERROR_MESSAGE);
+	        }
+		
+	}
 
 }
