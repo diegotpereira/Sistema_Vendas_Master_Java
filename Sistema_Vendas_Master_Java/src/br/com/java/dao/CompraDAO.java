f5d2630246ebd554582abfa44668009e0b6881b3
Diego <diegoteixeirapereira@hotmail.com>
2020-09-28 09:22:54 -0300

    alteração no arquivo classe CompraDAO

--- Sistema_Vendas_Master_Java/src/br/com/java/dao/CompraDAO.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/dao/CompraDAO.java
@@ -1,8 +1,41 @@
 package br.com.java.dao;
 
+import java.util.ArrayList;
+
 import br.com.java.dao.IDAO;
+import br.com.java.to.Compra;
 
 
 public class CompraDAO implements IDAO<Compra> {
+
+	@Override
+	public void inserir(Compra objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alterar(Compra objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Compra objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Compra> listarTodos() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Compra recuperar(int codigo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
