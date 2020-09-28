3155385762705a6ffb1950286e42158b8de8d499
Diego <diegoteixeirapereira@hotmail.com>
2020-09-28 10:05:45 -0300

    alteração no arquivo classe Compra.java

--- Sistema_Vendas_Master_Java/src/br/com/java/to/Compra.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/to/Compra.java
@@ -19,65 +19,83 @@ public class Compra {
     private Situacao situacao;
     private List<ItemCompra> itens;
     private List<ItemCompra> itensRemover;
-	public int getCodigo() {
-		return codigo;
-	}
+
 	
-	   public Compra() {
-	        this.codigo = 0;
-	        this.fornecedor = new Fornecedor();
-	        this.dataCompra = new Date();
-	        this.valorTotal = 0.0;
-	        this.itens = new ArrayList<>();
-	        this.itensRemover = new ArrayList<>();
+	  public int getCodigo() {
+	        return codigo;
 	    }
 
-	    public Compra(int codigo) {
-	        this.codigo = codigo;
-	        this.fornecedor = new Fornecedor();
-	        this.dataCompra = new Date();
-	        this.valorTotal = 0.0;
-	        this.itens = new ArrayList<>();
-	        this.itensRemover = new ArrayList<>();
-
-	    }
 	    public void setCodigo(int codigo) {
-			this.codigo = codigo;
-		}
-		public Fornecedor getFornecedor() {
-			return fornecedor;
-		}
-		public void setFornecedor(Fornecedor fornecedor) {
-			this.fornecedor = fornecedor;
-		}
-		public Date getDataCompra() {
-			return dataCompra;
-		}
-		public void setDataCompra(Date dataCompra) {
-			this.dataCompra = dataCompra;
-		}
-		public Double getValorTotal() {
-			return valorTotal;
-		}
-		public void setValorTotal(Double valorTotal) {
-			this.valorTotal = valorTotal;
-		}
-		public Situacao getSituacao() {
-			return situacao;
-		}
-		public void setSituacao(Situacao situacao) {
-			this.situacao = situacao;
-		}
-		public List<ItemCompra> getItens() {
-			return itens;
-		}
-		public void setItens(List<ItemCompra> itens) {
-			this.itens = itens;
-		}
-		public List<ItemCompra> getItensRemover() {
-			return itensRemover;
-		}
-		public void setItensRemover(List<ItemCompra> itensRemover) {
-			this.itensRemover = itensRemover;
-		}
+	        this.codigo = codigo;
+	    }
+
+	    public Fornecedor getFornecedor() {
+	        return fornecedor;
+	    }
+
+	    public void setFornecedor(Fornecedor fornecedor) {
+	        this.fornecedor = fornecedor;
+	    }
+
+	    public Date getDataCompra() {
+	        return dataCompra;
+	    }
+
+	    public void setDataCompra(Date dataCompra) {
+	        this.dataCompra = dataCompra;
+	    }
+
+	    public Double getValorTotal() {
+	        double total = 0;
+	        for (ItemCompra iv : itens) {
+	            total += (iv.getValorUnitario() * iv.getQuantidade());
+	        }
+	        return total;
+	    }
+
+	    public Situacao getSituacao() {
+	        return situacao;
+	    }
+
+	    public void setSituacao(Situacao situacao) {
+	        this.situacao = situacao;
+	    }
+
+	    public void setSituacao(int situacao) {
+	        if (situacao == Situacao.ABERTA.getId()) {
+	            setSituacao(Situacao.ABERTA);
+	        } else if (situacao == Situacao.FINALIZADA.getId()) {
+	            setSituacao(Situacao.FINALIZADA);
+	        } else if (situacao == Situacao.CANCELADA.getId()) {
+	            setSituacao(Situacao.CANCELADA);
+	        }
+	    }
+
+	    public List<ItemCompra> getItens() {
+	        return itens;
+	    }
+
+	    public List<ItemCompra> getItensRemover() {
+	        return itensRemover;
+	    }
+
+	    public void addItem(ItemCompra itemCompra) {
+	        itens.add(itemCompra);
+	    }
+
+	    public void removeItem(ItemCompra itemCompra) {
+	        itens.remove(itemCompra);
+	        if (itemCompra.getCodigo() != 0) {
+	            itensRemover.add(itemCompra);
+	        }
+	    }
+
+	    public int quantidadeItens() {
+	        return itens.size();
+	    }
+
+	    @Override
+	    public String toString() {
+	        return String.valueOf(this.codigo);
+	    }
 }
