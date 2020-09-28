3bd685926bd6e03e9ff94ef72b23413aa4530a05
Diego <diegoteixeirapereira@hotmail.com>
2020-09-28 09:01:26 -0300

    alteração no arquivo ClienteDAO criação dos metódos

--- Sistema_Vendas_Master_Java/src/br/com/java/dao/ClienteDAO.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/dao/ClienteDAO.java
@@ -1,8 +1,40 @@
 package br.com.java.dao;
 
+import java.util.ArrayList;
+
 import br.com.java.dao.IDAO;
 import br.com.java.to.Cliente;
 
 public class ClienteDAO implements IDAO<Cliente> {
+
+	@Override
+	public void inserir(Cliente objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void alterar(Cliente objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void excluir(Cliente objeto) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ArrayList<Cliente> listarTodos() throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Cliente recuperar(int codigo) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
