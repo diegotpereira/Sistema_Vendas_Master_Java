e46d40a79d26da8ccf5435568631f9379d5f1c3a
T.I <T.I@TI.rsatacadao.local>
2020-09-28 12:17:09 -0300

    alteração no arquivo classe Sobre.java modificação no texto da descrição do projeto.

--- Sistema_Vendas_Master_Java/src/br/com/java/gui/Sobre.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/gui/Sobre.java
@@ -9,8 +9,41 @@ public class Sobre extends javax.swing.JDialog {
 
 	private void initComponents() {
 		// TODO Auto-generated method stub
+		 java.awt.GridBagConstraints gridBagConstraints;
+
+	        lbIcone = new javax.swing.JLabel();
+	        taTexto = new javax.swing.JTextArea();
+
+	        setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+	        setTitle("Sobre");
+	        setResizable(false);
+	        getContentPane().setLayout(new java.awt.GridBagLayout());
+
+	        lbIcone.setIcon(new javax.swing.ImageIcon(getClass().getResource("/br/com/java/gui/img/sobre.png"))); // NOI18N
+	        gridBagConstraints = new java.awt.GridBagConstraints();
+	        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+	        gridBagConstraints.weighty = 1.0;
+	        gridBagConstraints.insets = new java.awt.Insets(0, 20, 0, 20);
+	        getContentPane().add(lbIcone, gridBagConstraints);
+
+	        taTexto.setEditable(false);
+	        taTexto.setColumns(20);
+	        taTexto.setFont(new java.awt.Font("SansSerif", 0, 11)); // NOI18N
+	        taTexto.setRows(5);
+	        taTexto.setText("Sistema de Vendas\nAutor: Diego Teixeira Pereira\nE-mail: diegoteixeirapereira@hotmail.com\n\nAplicação desenvolvida como material\nde apoio ao aprendizado de programação \nJava.");
+	        taTexto.setBorder(javax.swing.BorderFactory.createEmptyBorder(10, 10, 10, 10));
+	        taTexto.setOpaque(false);
+	        gridBagConstraints = new java.awt.GridBagConstraints();
+	        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+	        gridBagConstraints.weightx = 1.0;
+	        gridBagConstraints.weighty = 1.0;
+	        getContentPane().add(taTexto, gridBagConstraints);
+
+	        setSize(new java.awt.Dimension(330, 220));
+	        setLocationRelativeTo(null);
 		
 	}
-	
+	  private javax.swing.JLabel lbIcone;
+	  private javax.swing.JTextArea taTexto;
 
 }
