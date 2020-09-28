6941ef82fce585f998a7a461409109afdbf751bb
Diego <diegoteixeirapereira@hotmail.com>
2020-09-28 09:04:36 -0300

    alteração no arquivo cliente criação dos atributos

--- Sistema_Vendas_Master_Java/src/br/com/java/to/Cliente.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/to/Cliente.java
@@ -1,5 +1,38 @@
 package br.com.java.to;
 
+import java.util.Date;
+
 public class Cliente {
+	
+    private int codigo;
+    private String nome;
+    private String cpf;
+    private Date dataNascimento;
+	public int getCodigo() {
+		return codigo;
+	}
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCpf() {
+		return cpf;
+	}
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+	public Date getDataNascimento() {
+		return dataNascimento;
+	}
+	public void setDataNascimento(Date dataNascimento) {
+		this.dataNascimento = dataNascimento;
+	}
+    
+    
 
 }
