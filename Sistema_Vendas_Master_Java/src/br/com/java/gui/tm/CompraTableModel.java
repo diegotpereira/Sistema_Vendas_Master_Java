96d3bc25900b102661c99ad6df91c0f9bcd56f36
Diego <diegoteixeirapereira@hotmail.com>
2020-09-28 15:55:50 -0300

    alteração no arquivo classe CompraTableModel.java Adicionado atributos e metodos

--- Sistema_Vendas_Master_Java/src/br/com/java/gui/tm/CompraTableModel.java
+++ Sistema_Vendas_Master_Java/src/br/com/java/gui/tm/CompraTableModel.java
@@ -1,5 +1,69 @@
 package br.com.java.gui.tm;
 
-public class CompraTableModel {
+import java.text.NumberFormat;
+import java.text.SimpleDateFormat;
+import java.util.List;
+
+import javax.swing.table.AbstractTableModel;
+
+import br.com.java.to.Compra;
+
+
+
+public class CompraTableModel extends AbstractTableModel {
+	
+	private final SimpleDateFormat SDF = new SimpleDateFormat("dd/MM/yyyy");
+    private final NumberFormat NFC = NumberFormat.getCurrencyInstance();
+    private String colunas[] = {"Fornecedor", "Data", "Valor", "Situação"};
+    private List<Compra> dados;
+
+    @Override
+    public int getRowCount() {
+        if(dados == null){
+            return 0;
+        }
+        return dados.size();
+    }
+
+    @Override
+    public int getColumnCount() {
+        return colunas.length;
+    }
+
+    @Override
+    public Object getValueAt(int l, int c) {
+        Compra compra = dados.get(l);
+        switch (c) {
+            case 0:
+                return compra.getFornecedor().getNome();
+            case 1:
+                return SDF.format(compra.getDataCompra());
+            case 2:
+                return NFC.format(compra.getValorTotal());
+            case 3:
+                return compra.getSituacao();
+            default:
+                throw new IndexOutOfBoundsException("Coluna inexistente!");
+        }
+    }
+
+    @Override
+    public String getColumnName(int c) {
+        return colunas[c];
+    }
+
+    @Override
+    public boolean isCellEditable(int l, int c) {
+        return false;
+    }
+
+    public void setDados(List<Compra> dados) {
+        this.dados = dados;
+        fireTableDataChanged();
+    }
+
+    public Compra getRowValue(int l) {
+        return dados.get(l);
+    }
 
 }
